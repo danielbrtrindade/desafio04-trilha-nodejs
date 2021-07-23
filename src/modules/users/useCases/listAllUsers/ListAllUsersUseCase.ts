ee2c8f4f3ff146857cc9c356d28852b9c87eb04a
Daniel Trindade <daniel_brt@hotmail.com>
2021-07-22 21:14:51 -0300

    Finishing challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,18 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const { admin } = this.usersRepository.findById(user_id);
+
+    if (!admin) {
+      throw new Error("User without permission!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
